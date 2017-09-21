4c660a319706412b5b5e4931f1d57e6d11121890
Adam Abonyi <abonyi@seznam.cz>
2017-09-22 00:14:27 +0200

    First working something

--- ScrapeReddit/index.js
+++ ScrapeReddit/index.js
@@ -1,35 +1,40 @@
-var snoowrap = require('snoowrap');
+const snoowrap = require('snoowrap');
 
 module.exports = function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
     var r = new snoowrap({
-        user_agent: "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.91 Safari/537.36",
+        user_agent: "Azure Functions automatic downloader v0.1 (by /u/Xenik )",
         client_id: process.env["ClientId"],
         client_secret: process.env["ClientSecret"],
-        username: process.env["Username"],
+        username: "xenik",
         password: process.env["Password"]
-      });
+    });
 
-      var result = [];
-      var me = "";
-      //var result = r.getSubreddit('snoowrap').getHot()
-      //var result = r.getMe();
-      if (req.query.name || (req.body && req.body.name)) {
+    var result = [];
+    var me = "";
+    var result = r.getMe().then(function(x) {
         context.res = {
+            
+                        // status: 200, /* Defaults to 200 */
+                        body: "Hello " + x + (req.query.name || req.body.name)
+                    };
+        context.done();
+    });
+    //var result = r.getMe();
+    // if (req.query.name || (req.body && req.body.name)) {
+    //     context.res = {
 
-            // status: 200, /* Defaults to 200 */
-            body: "Hello " + (req.query.name || req.body.name)
-        };
-    }
-    else {
-        context.res = {
-            status: 400,
-            body: "Please pass a name on the query string or in the request body"
-        };
-    }
-    //   r.getMe().then(function(m) { 
-    //       me = m
-    //     });
-    //     me = r.getSubmission('2np694').body;
+    //         // status: 200, /* Defaults to 200 */
+    //         body: "Hello " + (req.query.name || req.body.name)
+    //     };
+    // }
+    // else {
+    //     context.res = {
+    //         status: 400,
+    //         body: "APlease pass a name on the query string or in the request body"
+    //     };
+    // }
+    //context.done();
+   
 };
