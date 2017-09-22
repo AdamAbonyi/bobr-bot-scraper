8625710a3cb303e554dad9ebf70cdcf3562d59fb
Adam Abonyi <abonyi@seznam.cz>
2017-09-22 23:10:45 +0200

    Cleaning up

--- ScrapeReddit/index.js
+++ ScrapeReddit/index.js
@@ -61,22 +61,4 @@ module.exports = function (context, req) {
         }
         context.done();
     });
-
-
-    //var result = r.getMe();
-    // if (req.query.name || (req.body && req.body.name)) {
-    //     context.res = {
-
-    //         // status: 200, /* Defaults to 200 */
-    //         body: "Hello " + (req.query.name || req.body.name)
-    //     };
-    // }
-    // else {
-    //     context.res = {
-    //         status: 400,
-    //         body: "APlease pass a name on the query string or in the request body"
-    //     };
-    // }
-    //context.done();
-
 };
