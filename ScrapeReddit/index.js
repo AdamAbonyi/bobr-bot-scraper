c3e1ba4bbf47a127f521622619b5f449da9b545e
Adam Abonyi <abonyi@seznam.cz>
2017-09-22 23:09:01 +0200

    Version that supports subreddit name as a parameter and outputs first iframe output or picture source

--- ScrapeReddit/index.js
+++ ScrapeReddit/index.js
@@ -16,19 +16,47 @@ module.exports = function (context, req) {
         body: "Please pass a subreddit name on the query string or in the request body"
     }; 
 
-    if (!(req.body && req.body.subreddit)) {
+
+    var subreddit = null;
+    if (!!req.body) subreddit = req.body.subreddit;
+    if (!subreddit && !!req.query) subreddit = req.query.subreddit;
+
+    if (!subreddit) {
         context.res = {
             status: 400,
             body: "Please pass a subreddit name on the query string or in the request body"
         };
     }
 
-    r.getSubreddit('gifs').getHot().then((x) => {
-        if (!!x && x.length > 0 && (req.body && req.body.subreddit)) {
-            let first = x.find(x => !x.stickied && !x.pinned);
+    function checkForEmbeded(item) {
+        if (!!item["secure_media"]  && !!item["secure_media"]["oembed"] && !!item["secure_media"]["oembed"].html) {
+            var str = item["secure_media"]["oembed"].html;
+
+            var regex = /<iframe.*?src=['"](.*?)['"]/;
+            var src = regex.exec(str)[1];
+
+            return src;
+        }
+
+        return null;
+    }
+
+    function checkForPicture(item) {
+        if (!!item["preview"]  && !!item["preview"]["images"] && !!item["preview"]["images"][0] && !!item["preview"]["images"][0].source && !!item["preview"]["images"][0].source.url) {
+            return item["preview"]["images"][0].source.url;
+        }
+
+        return null;
+    }
+
+    r.getSubreddit(subreddit).getHot().then((x) => {
+        if (!!x && x.length > 0 && !!subreddit) {
+            let first = x.find(x => !x.stickied && !x.pinned && (!!checkForEmbeded(x) || checkForPicture(x)) );
+            let result = checkForEmbeded(first);
+            if (!result) result = checkForPicture(first);
             context.res = {
                 // status: 200, /* Defaults to 200 */
-                body: JSON.stringify(first.body || x[0])
+                body: JSON.stringify(result)
             };
         }
         context.done();
